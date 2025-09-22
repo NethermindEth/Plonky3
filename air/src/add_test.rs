a475b257c6586e2ef044eb16b0427f8137241be1
Ilia Vlasov <elijah.vlasov@gmail.com>
2025-09-22 17:59:08 +0100

    GOT IT!

--- air/src/add_test.rs
+++ air/src/add_test.rs
@@ -2,7 +2,9 @@ use p3_baby_bear::BabyBear;
 use p3_field::*;
 use p3_matrix::Matrix;
 
-use crate::{air, symbolic_builder::SymbolicAirBuilder, Air, AirBuilder, AirBuilderWithPublicValues, BaseAir};
+use crate::{
+    Air, AirBuilder, AirBuilderWithPublicValues, BaseAir, air, symbolic_builder::SymbolicAirBuilder,
+};
 
 pub struct FibonacciAir {}
 
@@ -14,34 +16,27 @@ impl<F> BaseAir<F> for FibonacciAir {
     }
 }
 
-
-
 impl<AB: AirBuilder> Air<AB> for FibonacciAir {
     fn eval(&self, builder: &mut AB) {
         let main = builder.main();
 
-        let (local) = (
-            main.row_slice(0).expect("Matrix is empty?")
-        );
-        
+        let (local) = (main.row_slice(0).expect("Matrix is empty?"));
+
+        let x = <AB as air::AirBuilder>::Expr::from(local[2]);
         // a + b = c + 2^8 * r
-        builder.assert_eq::<<AB as air::AirBuilder>::Expr, <AB as air::AirBuilder>::Expr>
-            (
-                local[0] + local[1],
-                <AB as air::AirBuilder>::Expr::from(local[2]) + 
-                 256u32.into() * <AB as air::AirBuilder>::Expr::from(local[3])
-            );
-        
-
-
+        let y = local[2];
+        builder.assert_eq::<<AB as air::AirBuilder>::Expr, <AB as air::AirBuilder>::Expr>(
+            local[0] + local[1],
+            (<AB as air::AirBuilder>::Expr::from(local[3]) * AB::F::from_u32(256)) + y,
+        );
     }
 }
 
 #[test]
 fn extract_fib() {
     let air = FibonacciAir {};
-    let mut sbuilder  = 
-        SymbolicAirBuilder::<BabyBear, BabyBear, ()>::new(0, 2, 0, 0);
+    let mut sbuilder = SymbolicAirBuilder::<BabyBear, BabyBear, ()>::new(0, 2, 0, 0);
     air.eval(&mut sbuilder);
     sbuilder.print_lean_constraints();
 }
+
